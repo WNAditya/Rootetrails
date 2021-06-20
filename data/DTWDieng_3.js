7dc9ceb7060e3506cd7ac90db8bc9238429f587d
WNAditya <86209663+WNAditya@users.noreply.github.com>
2021-06-21 06:12:21 +0700

    Add files via upload

---
+++ data/DTWDieng_3.js
@@ -0,0 +1,21 @@
+var json_DTWDieng_3 = {
+"type": "FeatureCollection",
+"name": "DTWDieng_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Telaga Cebong, Sawah & Hutan, Sembungan, Wonosobo Regency, Central Java", "descriptio": null, "Visual": "Visual Asset\/Telaga Cebong\/Telaga_Cebong_Cover.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.921903893238962, -7.237387944687335 ] } },
+{ "type": "Feature", "properties": { "Name": "Bukit Sikunir Dieng, kebun & Hutan, Sembungan, Wonosobo Regency, Central Java", "descriptio": null, "Visual": "Visual Asset\/Bukit Sikunir\/Bukit_Sikunir_3.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.924769120060986, -7.239120493822404 ] } },
+{ "type": "Feature", "properties": { "Name": "Dieng Plateau Theater, Dieng, Wonosobo Regency, Central Java", "descriptio": null, "Visual": "Visual Asset\/Dieng Plateau Theater\/Dieng_Plateau_Theater_Cover.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.9145506, -7.2168151 ] } },
+{ "type": "Feature", "properties": { "Name": "Telaga Warna, Dieng, Wonosobo Regency, Central Java", "descriptio": null, "Visual": "Visual Asset\/Telaga Warna & Telaga Pengilon\/Telaga_Warna_Pengilon_5.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.913773845188658, -7.214773890734862 ] } },
+{ "type": "Feature", "properties": { "Name": "Batu Pandang Ratapan Angin, Jalan Dieng, Dieng, Wonosobo Regency, Central Java", "descriptio": null, "Visual": "Visual Asset\/Batu Ratapan Angin\/Batu_Ratapan_Angin_Cover.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.916451192820332, -7.217773006445269 ] } },
+{ "type": "Feature", "properties": { "Name": "Kawah Sikidang, Bakal Buntu, Dieng Kulon, Banjarnegara, Central Java", "descriptio": null, "Visual": "Kawah_Sikidang_Cover.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.90406658444104, -7.220314405389895 ] } },
+{ "type": "Feature", "properties": { "Name": "Candi Arjuna, Karangsari, Dieng Kulon, Banjarnegara, Central Java", "descriptio": null, "Visual": "Visual Asset\/Candi Arjuna\/Candi_Arjuna_Cover.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.906971739624382, -7.205103565569904 ] } },
+{ "type": "Feature", "properties": { "Name": "Tuk Bimolukar, Kalilembu, Dieng, Wonosobo Regency, Central Java", "descriptio": null, "Visual": "Visual Asset\/Tuk Bimalukar\/Tuk_Bimalukar_Cover.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.91319800088209, -7.204753732853941 ] } },
+{ "type": "Feature", "properties": { "Name": "Bukit Scooter Dieng, Simpangan, Dieng, Banjarnegara, Central Java", "descriptio": null, "Visual": "Visual Asset\/Bukit Scooter\/Bukit_Scooter_Cover.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.906068428450581, -7.197953731288985 ] } },
+{ "type": "Feature", "properties": { "Name": "Batu Tulis", "descriptio": null, "Visual": "Visual Asset\/Telaga Warna & Telaga Pengilon\/Telaga_Warna_Pengilon_3.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.915564098810208, -7.214628340043268 ] } },
+{ "type": "Feature", "properties": { "Name": "Gua Sumur", "descriptio": null, "Visual": "Visual Asset\/Telaga Warna & Telaga Pengilon\/Telaga_Warna_Pengilon_1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.916068498069677, -7.214475571911668 ] } },
+{ "type": "Feature", "properties": { "Name": "Telaga Pengilon", "descriptio": null, "Visual": "Visual Asset\/Telaga Warna & Telaga Pengilon\/Telaga_Warna_Pengilon_2.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.91718035893723, -7.214014861407301 ] } },
+{ "type": "Feature", "properties": { "Name": "IGM Shop Pusat Carica dan Oleh-Oleh Khas Dieng", "descriptio": null, "Visual": null }, "geometry": { "type": "Point", "coordinates": [ 109.928003304233911, -7.214317327827914 ] } },
+{ "type": "Feature", "properties": { "Name": "RM HD Gardu Pandang", "descriptio": null, "Visual": "Visual Asset\/RM HD Gardu Pandang\/Gardu_Pandang_Cover.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.938202839258736, -7.233284246204334 ] } }
+]
+}
---
+++ data/Fasilitas_2.js
@@ -0,0 +1,20 @@
+var json_Fasilitas_2 = {
+"type": "FeatureCollection",
+"name": "Fasilitas_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Nama": "PKU Muhammadiyah Tieng", "Jenis": "Health Facility" }, "geometry": { "type": "Point", "coordinates": [ 109.943189343593275, -7.233973083394089 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Masjid Nurul Ummah", "Jenis": "Worship Place" }, "geometry": { "type": "Point", "coordinates": [ 109.937310387056712, -7.235411878250687 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Mushola Assalam Sikunir", "Jenis": "Worship Place" }, "geometry": { "type": "Point", "coordinates": [ 109.923254607723095, -7.23654425986583 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Masjid Raudlatul Muttaqin", "Jenis": "Worship Place" }, "geometry": { "type": "Point", "coordinates": [ 109.916780294928131, -7.23448080679413 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Mushola Bukit Sikunir", "Jenis": "Worship Place" }, "geometry": { "type": "Point", "coordinates": [ 109.926029206626652, -7.238025973216573 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Masjid Siterus", "Jenis": "Worship Place" }, "geometry": { "type": "Point", "coordinates": [ 109.910464893014193, -7.220908261061086 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Masjid Parkiran Telaga Warna", "Jenis": "Worship Place" }, "geometry": { "type": "Point", "coordinates": [ 109.913353639170197, -7.21514990881004 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "UPTD Puskesmas Kejajar II", "Jenis": "Health Facility" }, "geometry": { "type": "Point", "coordinates": [ 109.911309433470961, -7.203570810143741 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Puskesmas Batur 2", "Jenis": "Health Facility" }, "geometry": { "type": "Point", "coordinates": [ 109.90268049802647, -7.20294610476851 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "SPBU Pertamina 44 534 04", "Jenis": "Petrol Station" }, "geometry": { "type": "Point", "coordinates": [ 109.902453695642322, -7.202883930252781 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Mushola Al-Amin", "Jenis": "Worship Place" }, "geometry": { "type": "Point", "coordinates": [ 109.909554699235656, -7.202632271411545 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Pos Polisi Dieng", "Jenis": "Security Facility" }, "geometry": { "type": "Point", "coordinates": [ 109.908881752687904, -7.202490158121837 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Masjid Al-Fattah", "Jenis": "Worship Place" }, "geometry": { "type": "Point", "coordinates": [ 109.922139247314178, -7.208002936824868 ] } }
+]
+}
